e6714fb944204bf3d817af3b68154b180289de95
misha-fedyna <mihajlofedina5@gmail.com>
2025-04-01 17:02:41 +0300

    Виправив hydration error

--- components/cars/CarFilter.jsx
+++ components/cars/CarFilter.jsx
@@ -38,6 +38,11 @@ const CarFilter = () => {
     "Ліфтбек",
     "Кросовер-купе",
   ];
+
+  const gearboxs = ["Автоматична", "Механічна"];
+
+  const cardrives = ["Повний", "Передній", "Задній"];
+
   return (
     <>
       {/* Laptop */}
@@ -70,24 +75,17 @@ const CarFilter = () => {
             <AccordionTrigger>Коробка передач</AccordionTrigger>
             <AccordionContent>
               <div className="flex flex-col gap-[10px]">
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="auto" />
-                  <Label
-                    htmlFor="auto"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Автоматична
-                  </Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="mechanic" />
-                  <Label
-                    htmlFor="mechanic"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Механічна
-                  </Label>
-                </div>
+                {gearboxs.map((gearbox, index) => (
+                  <div key={index} className="flex items-center space-x-2">
+                    <Checkbox id={gearbox} />
+                    <Label
+                      htmlFor={gearbox}
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      {gearbox}
+                    </Label>
+                  </div>
+                ))}
               </div>
             </AccordionContent>
           </AccordionItem>
@@ -96,33 +94,17 @@ const CarFilter = () => {
             <AccordionTrigger>Привід</AccordionTrigger>
             <AccordionContent>
               <div className="flex flex-col gap-[10px]">
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="full" />
-                  <Label
-                    htmlFor="full"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Повний
-                  </Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="zadniy" />
-                  <Label
-                    htmlFor="zadniy"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Задній
-                  </Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="peredniy" />
-                  <Label
-                    htmlFor="peredniy"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Передній
-                  </Label>
-                </div>
+                {cardrives.map((cardrive, index) => (
+                  <div key={index} className="flex items-center space-x-2">
+                    <Checkbox id={cardrive} />
+                    <Label
+                      htmlFor={cardrive}
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      {cardrive}
+                    </Label>
+                  </div>
+                ))}
               </div>
             </AccordionContent>
           </AccordionItem>
@@ -180,135 +162,116 @@ const CarFilter = () => {
               <SheetTitle className="text-blue-500 text-3xl w-5 mb-2">
                 Фільтр
               </SheetTitle>
-              <SheetDescription>
-                <div className="flex flex-col w-[250px] mx-2">
-                  <CarState />
-                  <Accordion type="multiple">
-                    {/* Тип кузова */}
-                    <AccordionItem value="item-1">
-                      <AccordionTrigger>Тип кузова</AccordionTrigger>
-                      <AccordionContent>
-                        <div className="flex flex-col gap-[10px]">
-                          {types.map((type, index) => (
-                            <div
-                              key={`mob-${index}`}
-                              className="flex items-center space-x-2"
-                            >
-                              <Checkbox id={type} />
-                              <Label
-                                htmlFor={type}
-                                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                              >
-                                {type}
-                              </Label>
-                            </div>
-                          ))}
+              {/* <SheetDescription> */}
+              <CarState />
+              {/* </SheetDescription> */}
+              <Accordion type="multiple">
+                {/* Тип кузова */}
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>Тип кузова</AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col gap-[10px]">
+                      {types.map((type, index) => (
+                        <div
+                          key={`mob-${index}`}
+                          className="flex items-center space-x-2"
+                        >
+                          <Checkbox id={type} />
+                          <Label
+                            htmlFor={type}
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                          >
+                            {type}
+                          </Label>
                         </div>
-                      </AccordionContent>
-                    </AccordionItem>
-                    {/* Коробка передач */}
-                    <AccordionItem value="item-2">
-                      <AccordionTrigger>Коробка передач</AccordionTrigger>
-                      <AccordionContent>
-                        <div className="flex flex-col gap-[10px]">
-                          <div className="flex items-center space-x-2">
-                            <Checkbox id="auto" />
-                            <Label
-                              htmlFor="auto"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Автоматична
-                            </Label>
-                          </div>
-                          <div className="flex items-center space-x-2">
-                            <Checkbox id="mechanic" />
-                            <Label
-                              htmlFor="mechanic"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Механічна
-                            </Label>
-                          </div>
-                        </div>
-                      </AccordionContent>
-                    </AccordionItem>
-                    {/* Привід */}
-                    <AccordionItem value="item-3">
-                      <AccordionTrigger>Привід</AccordionTrigger>
-                      <AccordionContent>
-                        <div className="flex flex-col gap-[10px]">
-                          <div className="flex items-center space-x-2">
-                            <Checkbox id="full" />
-                            <Label
-                              htmlFor="full"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Повний
-                            </Label>
-                          </div>
-                          <div className="flex items-center space-x-2">
-                            <Checkbox id="zadniy" />
-                            <Label
-                              htmlFor="zadniy"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Задній
-                            </Label>
-                          </div>
-                          <div className="flex items-center space-x-2">
-                            <Checkbox id="peredniy" />
-                            <Label
-                              htmlFor="peredniy"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Передній
-                            </Label>
-                          </div>
-                        </div>
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
-                  {/* Ціна, регіон, марка */}
-                  <div className="flex flex-col gap-[10px] mt-[16px]">
-                    {/* Марка */}
-                    <ChooseMarks />
-
-                    {/* Регіон */}
-                    <ChooseRegions />
-
-                    {/* Ціна */}
-                    <h1 className="font-medium max-sm:flex max-sm:justify-start">
-                      Ціна
-                    </h1>
-                    <div className="flex items-center space-x-2">
-                      <Input
-                        className="h-[30px] w-[90px] border-b-2"
-                        type="number"
-                        placeholder="8 500"
-                      />
-                      <Input
-                        className="h-[30px] w-[90px] border-b-2"
-                        type="number"
-                        placeholder="20 000"
-                      />
-                      <Select>
-                        <SelectTrigger className="w-[50px] h-[30px]">
-                          <SelectValue placeholder="$" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectItem value="dolar">$</SelectItem>
-                          <SelectItem value="euro">€</SelectItem>
-                          <SelectItem value="hrn">₴</SelectItem>
-                        </SelectContent>
-                      </Select>
+                      ))}
                     </div>
-                  </div>
-                  <div className="w-[100%] justify-items-center">
-                    <Button className="flex align-middle mt-[20px] text-2xl w-full bg-blue-700 hover:bg-blue-300">
-                      Застосувати
-                    </Button>
-                  </div>
+                  </AccordionContent>
+                </AccordionItem>
+                {/* Коробка передач */}
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>Коробка передач</AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col gap-[10px]">
+                      {gearboxs.map((gearbox, index) => (
+                        <div
+                          key={`mob-${index}`}
+                          className="flex items-center space-x-2"
+                        >
+                          <Checkbox id={gearbox} />
+                          <Label
+                            htmlFor={gearbox}
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                          >
+                            {gearbox}
+                          </Label>
+                        </div>
+                      ))}
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                {/* Привід */}
+                <AccordionItem value="item-3">
+                  <AccordionTrigger>Привід</AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col gap-[10px]">
+                      {cardrives.map((cardrive, index) => (
+                        <div
+                          key={`mob-${index}`}
+                          className="flex items-center space-x-2"
+                        >
+                          <Checkbox id={cardrive} />
+                          <Label
+                            htmlFor={cardrive}
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                          >
+                            {cardrive}
+                          </Label>
+                        </div>
+                      ))}
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+              {/* Ціна, регіон, марка */}
+              <div className="flex flex-col gap-[10px] mt-[16px]">
+                {/* Марка */}
+                <ChooseMarks />
+                {/* Регіон */}
+                <ChooseRegions />
+                {/* Ціна */}
+                <h1 className="font-medium max-sm:flex max-sm:justify-start">
+                  Ціна
+                </h1>
+                <div className="flex items-center space-x-2">
+                  <Input
+                    className="h-[30px] w-[90px] border-b-2"
+                    type="number"
+                    placeholder="8 500"
+                  />
+                  <Input
+                    className="h-[30px] w-[90px] border-b-2"
+                    type="number"
+                    placeholder="20 000"
+                  />
+                  <Select>
+                    <SelectTrigger className="w-[50px] h-[30px]">
+                      <SelectValue placeholder="$" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="dolar">$</SelectItem>
+                      <SelectItem value="euro">€</SelectItem>
+                      <SelectItem value="hrn">₴</SelectItem>
+                    </SelectContent>
+                  </Select>
                 </div>
+              </div>
+              <SheetDescription>
+                {/* <div className="w-[100%] justify-items-center"> */}
+                <Button className="flex align-middle mt-[20px] text-2xl w-full bg-blue-700 hover:bg-blue-300">
+                  Застосувати
+                </Button>
+                {/* </div> */}
               </SheetDescription>
             </SheetHeader>
           </SheetContent>
