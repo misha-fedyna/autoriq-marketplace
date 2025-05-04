c70499b2d62116c67ad277d2c78187806cebe0f2
elizabethzherebetska <zherebetska.yelizaveta@gmail.com>
2025-05-04 14:47:02 -0700

    footer fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google"; 
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/header/Navbar";
 import Footer from "@/components/footer/Footer";
@@ -21,12 +21,12 @@ export const metadata = {
 const MainLayout = ({ children }) => {
   return (
     <html lang="uk" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
